d54267e30a8ac1f82c58fc3ff75f5c5f74a0b3f2
alepape1 <alesantanag@gmail.com>
2023-05-11 00:49:27 +0100

    grod station app added

--- Firmware/Comunnication examples/lora_receiver/lora_receiver.ino
+++ Firmware/Comunnication examples/lora_receiver/lora_receiver.ino
@@ -1,6 +1,9 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+
+void decodePacket(unsigned char* buffer, size_t bufferSize);
+
 void setup() {
   Serial.begin(9600);
   while (!Serial);
@@ -14,20 +17,94 @@ void setup() {
 }
 
 void loop() {
+  
   // try to parse packet
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
     // received a packet
     
-    Serial.print("Received packet '");
-
-    // read packet
-    while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
+    // read packet into buffer
+    byte buffer[packetSize];
+    for (int i = 0; i < packetSize; i++) {
+      buffer[i] = LoRa.read();
     }
 
+    decodePacket(buffer,packetSize);
+    
     // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
+//    Serial.print("RSSI: ");
+//    Serial.print(LoRa.packetRssi());
+//    Serial.print(" dBm");
+//
+//    // print SNR of packet
+//    Serial.print(", SNR: ");
+//    Serial.print(LoRa.packetSnr());
+//    Serial.print(" dB");
+//    Serial.println();
   }
+}
+
+
+void decodePacket(unsigned char* buffer, size_t bufferSize) {
+  // Check start byte
+  if (buffer[0] != 0xFF) {
+    Serial.println("Invalid start byte");
+    return;
+  }
+  
+  // Extract data from buffer
+  int16_t roll = (buffer[1] << 8) | buffer[2];
+  int16_t pitch = (buffer[3] << 8) | buffer[4];
+  int16_t heading = (buffer[5] << 8) | buffer[6];
+  int16_t temperature = (buffer[7] << 8) | buffer[8];
+  int32_t pressure = (buffer[9] << 24) | (buffer[10] << 16) | (buffer[11] << 8) | buffer[12];
+  int32_t seaLevelPressure = (buffer[13] << 24) | (buffer[14] << 16) | (buffer[15] << 8) | buffer[16];
+  int32_t altitude = (buffer[17] << 24) | (buffer[18] << 16) | (buffer[19] << 8) | buffer[20];
+  int32_t correctedAltitude = (buffer[21] << 24) | (buffer[22] << 16) | (buffer[23] << 8) | buffer[24];
+  
+  // Check stop byte
+  if (buffer[25] != 0xFE) {
+    Serial.println("Invalid stop byte");
+    return;
+  }
+  
+  // Print extracted data to serial monitor
+//  Serial.print("Roll: ");
+//  Serial.print((float)roll / 10.0);
+//  Serial.print(", Pitch: ");
+//  Serial.print((float)pitch / 10.0);
+//  Serial.print(", Heading: ");
+//  Serial.print((float)heading);
+//  Serial.print(", Temperature: ");
+//  Serial.print((float)temperature / 10.0);
+//  Serial.print(", Pressure: ");
+//  Serial.print((float)pressure / 100.0);
+//  Serial.print(", Sea Level Pressure: ");
+//  Serial.print((float)seaLevelPressure / 100.0);
+//  Serial.print(", Altitude: ");
+//  Serial.print((float)altitude / 100.0);
+//  Serial.print(", Corrected Altitude: ");
+//  Serial.println((float)correctedAltitude / 100.0);
+
+  Serial.print((float)roll / 10.0);
+  Serial.print(",");
+  Serial.print((float)pitch / 10.0);
+  Serial.print(",");
+  Serial.print((float)heading);
+  Serial.print(",");
+  Serial.print((float)temperature / 10.0);
+  Serial.print(",");
+  Serial.print((float)pressure / 100.0);
+  Serial.print(",");
+  Serial.print((float)seaLevelPressure / 100.0);
+  Serial.print(",");
+  Serial.print((float)altitude / 100.0);
+  Serial.print(",");
+  Serial.print((float)correctedAltitude / 100.0);
+  Serial.print(",");
+  Serial.print(LoRa.packetRssi());
+  Serial.print(",");
+  Serial.print(LoRa.packetSnr());
+  Serial.println();
+
 }
--- Firmware/FW_FligthManager/Flight manager MKR13010/src/fligth_manager/fligth_manager.ino
+++ Firmware/FW_FligthManager/Flight manager MKR13010/src/fligth_manager/fligth_manager.ino
@@ -3,9 +3,15 @@
 #include <MS5x.h>
 #include <MKRIMU.h>
 #include <LoRa.h>
+#include "Seeed_BME280.h"
 
 // Initializes the barometer sensor and sets up the connection status variable
 MS5x barometer(&Wire);
+
+// Initizes the barometer sensor and Temperature for batteries
+//BME280 bme280;
+
+// Flag barometer status
 bool barometerConnected = false;
 
 // Stores the time of the last connection attempt, delay time between attempts, and the last time the device was polled
@@ -20,99 +26,185 @@ double prevTemperature = 0;
 // Stores the sea level pressure, used for altitude calculations
 double seaLevelPressure = 0;
 
+// Stores data IMU parameters
+float heading, roll, pitch;
+float pressure_batt = 0;
+
+bool barometer_init(void);
+void packData_and_send(float roll,float pitch,float heading,float temperature,float pressure,float seaLevelPressure,float altitude,float correctedAltitude);
+
 // Initializes the serial communication and the LoRa module
 void setup() {
-Serial.begin(9600);
+Serial.begin(115200);
 while (!Serial);
 
+// Init Pressure and temperature sensor BME280 ADDRESS (0x76)
+//if(!bme280.init()){.
+//    Serial.println("Device error!");
+//}
+
+// Init IMU inertial sensor
+if (!IMU.begin()) {
+   Serial.println("Failed to initialize IMU!");
+  }
+
+// Init LoRa RF
 if (!LoRa.begin(868E6)) {
 Serial.println("Starting LoRa failed!");
-while (1);
+
 }
 
+LoRa.setTxPower(10);
+LoRa.setSpreadingFactor(12);
 // Initializes the barometer sensor and sets up its settings
-barometer.setI2Caddr(I2C_LOW);
-barometer.setSamples(MS5xxx_CMD_ADC_2048);
-barometer.setDelay(1000);
-barometer.setPressMbar();
-barometer.setTOffset(-200);
-barometer.setPOffset(5);
-
-// Attempts to connect to the barometer sensor
-if (barometer.connect() > 0) {
-Serial.println(F("Error connecting..."));
-} else {
-Serial.println(F("Connected to Sensor"));
-barometerConnected = true;
-}
+barometerConnected = barometer_init();
 
 Serial.println("LoRa Sender");
 }
 
 void loop() {
-  // Read sensor data
-  float heading, roll, pitch, temperature, pressure, seaLevelPressure, altitude, correctedAltitude;
-  if (IMU.eulerAnglesAvailable()) {
+  
+  double pressure = 0;
+  double temperature = 0;
+  double altitude = 0;
+  double correctedAltitude = 0;
+  
+//  if(bme280.init()){
+//       //get and print temperatures
+//        Serial.print("Temp batt: ");
+//        Serial.print(bme280.getTemperature());
+//        Serial.print("C");//The unit for  Celsius because original arduino don't support speical symbols
+//        Serial.print(",");
+//        //get and print atmospheric pressure data
+//        Serial.print("Pressure batt: ");
+//        Serial.print(pressure = bme280.getPressure());
+//        Serial.print("Pa");
+//        Serial.print(",");
+//        //get and print altitude data
+//        Serial.print("Altitude batt: ");
+//        Serial.print(bme280.calcAltitude(pressure));
+//        Serial.print("m");
+//        Serial.print(",");
+//        //get and print humidity data
+//        Serial.print("Humidity batt: ");
+//        Serial.print(bme280.getHumidity());
+//        Serial.print("%");
+//        Serial.println();
+//        
+//    }else{
+//        Serial.println("Battery temperature sensor ERROR");
+//      }
+  
+  // Get the IMU absolute values on Euler angles
+  if (IMU.eulerAnglesAvailable()){
     IMU.readEulerAngles(heading, roll, pitch);
-
-    // Attempt to connect to barometer if not already connected
-    if (!barometerConnected) {
-      if (millis() - prevConnectionAttempt >= connectionAttemptDelay) {
-        if (barometer.connect()>0) {
-          Serial.println(F("Error connecting to barometer..."));
-          prevConnectionAttempt = millis();
-        } else {
-          Serial.println(F("Connected to barometer!"));
-          barometerConnected = true;
-        }
+  }
+  
+  // Attempt to connect to barometer if not already connected
+  if (!barometerConnected) {
+    if (millis() - prevConnectionAttempt >= connectionAttemptDelay) {
+      if (barometer.connect()>0) {
+        Serial.println(F("Error connecting to barometer..."));
+        prevConnectionAttempt = millis();
+      } else {
+        Serial.println(F("Connected to barometer!"));
+        barometerConnected = true;
       }
-    } else {
-      // Check if the barometer has new data available
-      barometer.checkUpdates();
-      if (barometer.isReady()) { 
-        temperature = barometer.GetTemp(); 
-        pressure = barometer.GetPres();
+    }
+  } else {
+    // Check if the barometer has new data available
+    barometer.checkUpdates();
+    if (barometer.isReady()) { 
+      temperature = barometer.GetTemp(); 
+      pressure = barometer.GetPres();
 
-        // If the temperature or pressure have changed, update the variables and calculate new altitude data
-        if ((temperature != prevTemperature) || (pressure != prevPressure)) {
-          if (seaLevelPressure == 0) seaLevelPressure = barometer.getSeaLevel(pressure);
-          altitude = barometer.getAltitude();
-          correctedAltitude = barometer.getAltitude(true);
+      // If the temperature or pressure have changed, update the variables and calculate new altitude data
+      if ((temperature != prevTemperature) || (pressure != prevPressure)) {
+        if (seaLevelPressure == 0) seaLevelPressure = barometer.getSeaLevel(pressure);
+        altitude = barometer.getAltitude();
+        correctedAltitude = barometer.getAltitude(true);
 
-          // Format sensor data into a string
-          String data = String(roll) + "," + String(pitch) + "," + String(heading) + "," + String(temperature) + "," + String(pressure) + "," + String(seaLevelPressure) + "," + String(altitude) + "," + String(correctedAltitude);
-         
-          // Create packet buffer with start and stop bytes, and add the formatted sensor data
-          unsigned char buffer[] = {
-            0xFF, // start byte
-            (unsigned char)(roll * 10),
-            (unsigned char)(pitch * 10),
-            (unsigned char)(heading * 10),
-            (unsigned char)(temperature + 128),
-            (unsigned char)(fmod(pressure, 256)),
-            (unsigned char)(fmod(seaLevelPressure, 256)),
-            (unsigned char)(fmod(altitude, 256)),
-            (unsigned char)(fmod(correctedAltitude, 256)),
-            (unsigned char)(altitude / 256),
-            (unsigned char)((int)altitude % 256),
-            (unsigned char)(correctedAltitude / 256),
-            (unsigned char)((int)correctedAltitude % 256),
-            0xFE // stop byte
-          };
-          
-          // Send packet via LoRa
-          LoRa.beginPacket();
-          LoRa.write(buffer, sizeof(buffer));
-          LoRa.endPacket();
-          Serial.println("Packet sent!");
+        // Format sensor data into a string
+        String data = "Roll:" + String(roll) + "," + "Pitch:" + String(pitch) + "," + "Heading:" + String(heading) + "," "Temperature:" + String(temperature) + "," + "Pressure:" + String(pressure) + "," + "SLp:" + String(seaLevelPressure) + "," + "Altitude:" + String(altitude) + "," + "C. Altitude" + String(correctedAltitude);
+        //String data = String(roll) + "," + String(pitch) + "," + String(heading) + "," + String(temperature) + "," + String(pressure) + "," + String(seaLevelPressure) + "," + String(altitude) + "," + String(correctedAltitude);
 
-          // Print the formatted sensor data to the serial monitor
-          Serial.print("Flight data: ");
-          Serial.print(data);
-          Serial.println();
-        }
+        packData_and_send(roll,pitch,heading,temperature,pressure,seaLevelPressure,altitude,correctedAltitude);
+        // Print the formatted sensor data to the serial monitor
+        Serial.print("Flight data: ");
+        Serial.print(data);
+//        Serial.println();
       }
     }
   }
-  delay(200);
+//  delay(10);
 }
+
+bool barometer_init(){
+
+    barometer.setI2Caddr(I2C_LOW);
+    barometer.setSamples(MS5xxx_CMD_ADC_4096);
+    barometer.setDelay(1000);
+    barometer.setPressMbar();                                 //GetPress() return Pressure in Milibars (default preassure units)
+    barometer.setTempC();                                     //GetTemp() return temperature in Celcius (default temperature units)
+//    barometer.setTOffset(-200);
+    barometer.setPOffset(5);
+    
+    // Attempts to connect to the barometer sensor
+    if (barometer.connect() > 0) {
+    Serial.println(F("Error connecting..."));
+    
+    return false;
+    
+    } else {
+    Serial.println(F("Connected to Baromete Sensor"));
+    
+    return true;
+    
+    }
+  
+  }
+
+
+void packData_and_send(float roll,float pitch,float heading,float temperature,float pressure,float seaLevelPressure,float altitude,float correctedAltitude) {
+  
+  // Convert roll and pitch to int16_t
+  int16_t rollInt = (int16_t)(roll * 10);
+  int16_t pitchInt = (int16_t)(pitch * 10);
+  
+  // Convert heading to int16_t
+  int16_t headingInt = (int16_t)heading;
+  
+  // Convert temperature to int16_t
+  int16_t temperatureInt = (int16_t)(temperature * 10);
+  
+  // Convert pressure to int32_t
+  int32_t pressureInt = (int32_t)(pressure * 100);
+  
+  // Convert sea level pressure, altitude, and corrected altitude to int32_t
+  int32_t seaLevelPressureInt = (int32_t)(seaLevelPressure * 100);
+  int32_t altitudeInt = (int32_t)(altitude * 100);
+  int32_t correctedAltitudeInt = (int32_t)(correctedAltitude * 100);
+  
+  // Create packet buffer with start and stop bytes, and add the formatted sensor data
+  unsigned char buffer[] = {
+    0xFF, // start byte
+    rollInt >> 8, rollInt & 0xFF,
+    pitchInt >> 8, pitchInt & 0xFF,
+    headingInt >> 8, headingInt & 0xFF,
+    temperatureInt >> 8, temperatureInt & 0xFF,
+    pressureInt >> 24, pressureInt >> 16, pressureInt >> 8, pressureInt & 0xFF,
+    seaLevelPressureInt >> 24, seaLevelPressureInt >> 16, seaLevelPressureInt >> 8, seaLevelPressureInt & 0xFF,
+    altitudeInt >> 24, altitudeInt >> 16, altitudeInt >> 8, altitudeInt & 0xFF,
+    correctedAltitudeInt >> 24, correctedAltitudeInt >> 16, correctedAltitudeInt >> 8, correctedAltitudeInt & 0xFF,
+    0xFE // stop byte
+  };
+    
+  // Send packet via LoRa
+  LoRa.beginPacket();
+  LoRa.write(buffer, sizeof(buffer));
+  LoRa.endPacket();
+  Serial.println("Packet sent!");
+
+}
+
+  
