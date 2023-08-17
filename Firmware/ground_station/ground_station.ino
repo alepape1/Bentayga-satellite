c5155a5504d211c74cae3df96adeddbecd338f18
alepape1 <alesantanag@gmail.com>
2023-08-17 19:19:50 +0100

    Added better visual for serial terminal in arduino for ground station

--- Firmware/ground_station/ground_station.ino
+++ Firmware/ground_station/ground_station.ino
@@ -5,7 +5,7 @@
 #include <LoRa.h>
 #include "Seeed_BME280.h"
 #include <Arduino_MKRGPS.h>
-#include "CRC32.h" // CRC32 by Christopher Baker
+//#include "CRC32.h" // CRC32 by Christopher Baker
 
 // Strucuture of data for Sensor data from the satellite
 struct SensorData {
@@ -35,7 +35,7 @@ struct SensorData {
   uint8_t minute;
   uint8_t second;
   uint8_t camera_info;
-  uint32_t checksum; // CRC32
+//  uint32_t checksum; // CRC32
 };
 
 // Initializes the serial communication and the LoRa module
@@ -67,63 +67,93 @@ void loop() {
     if (LoRa.readBytes((uint8_t*)&sensorData, sizeof(SensorData)) == sizeof(SensorData)) 
     {
       // Process the received sensor data
-      Serial.print(sensorData.roll);
-      Serial.print(",");
-      Serial.print(sensorData.pitch);
-      Serial.print(",");
-      Serial.print(sensorData.heading);
-      Serial.print(",");
-      Serial.print(sensorData.battTempLeft);
-      Serial.print(",");
-      Serial.print(sensorData.battTempRight);
-      Serial.print(",");
-      Serial.print(sensorData.battTempDown);
-      Serial.print(",");
-      Serial.print(sensorData.battTempBox);
-      Serial.print(",");
-      Serial.print(sensorData.TempCubesatDS18);
-      Serial.print(",");
-      Serial.print(sensorData.temperature);
-      Serial.print(",");
-      Serial.print(sensorData.pressure);
-      Serial.print(",");
-      Serial.print(sensorData.humidity);
-      Serial.print(",");
-      Serial.print(sensorData.GpsAltitude);
-      Serial.print(",");
-      Serial.print(sensorData.latitude);
-      Serial.print(",");
-      Serial.print(sensorData.longitude);
-      Serial.print(",");
-      Serial.print(sensorData.speed);
-      Serial.print(",");
-      Serial.print(sensorData.numSatellites);
-      Serial.print(",");
-      Serial.print(sensorData.year);
-      Serial.print(",");
-      Serial.print(sensorData.month);
-      Serial.print(",");
-      Serial.print(sensorData.day);
-      Serial.print(",");
-      Serial.print(sensorData.hour);
-      Serial.print(",");
-      Serial.print(sensorData.minute);
-      Serial.print(",");
-      Serial.print(sensorData.second);
-      Serial.print(",");
-      Serial.print(sensorData.camera_info);
-      Serial.print(",");
-      Serial.print(sensorData.checksum);
-      Serial.print(",");
-      Serial.print(LoRa.packetRssi());
-      Serial.print(",");
-      Serial.print(LoRa.packetSnr());
+//      Serial.print(sensorData.roll);
+//      Serial.print(",");
+//      Serial.print(sensorData.pitch);
+//      Serial.print(",");
+//      Serial.print(sensorData.heading);
+//      Serial.print(",");
+//      Serial.print(sensorData.battTempLeft);
+//      Serial.print(",");
+//      Serial.print(sensorData.battTempRight);
+//      Serial.print(",");
+//      Serial.print(sensorData.battTempDown);
+//      Serial.print(",");
+//      Serial.print(sensorData.battTempBox);
+//      Serial.print(",");
+//      Serial.print(sensorData.TempCubesatDS18);
+//      Serial.print(",");
+//      Serial.print(sensorData.temperature);
+//      Serial.print(",");
+//      Serial.print(sensorData.pressure);
+//      Serial.print(",");
+//      Serial.print(sensorData.humidity);
+//      Serial.print(",");
+//      Serial.print(sensorData.GpsAltitude);
+//      Serial.print(",");
+//      Serial.print(sensorData.latitude);
+//      Serial.print(",");
+//      Serial.print(sensorData.longitude);
+//      Serial.print(",");
+//      Serial.print(sensorData.speed);
+//      Serial.print(",");
+//      Serial.print(sensorData.numSatellites);
+//      Serial.print(",");
+//      Serial.print(sensorData.year);
+//      Serial.print(",");
+//      Serial.print(sensorData.month);
+//      Serial.print(",");
+//      Serial.print(sensorData.day);
+//      Serial.print(",");
+//      Serial.print(sensorData.hour);
+//      Serial.print(",");
+//      Serial.print(sensorData.minute);
+//      Serial.print(",");
+//      Serial.print(sensorData.second);
+//      Serial.print(",");
+//      Serial.print(sensorData.camera_info);
+//      Serial.print(",");
+//      Serial.print(sensorData.checksum);
+//      Serial.print(",");
 
-      checksum = CRC32::calculate((uint8_t *)&sensorData, sizeof(sensorData) - sizeof(sensorData.checksum));
+      Serial.print("Roll: "); Serial.print(sensorData.roll);
+      Serial.print("\tPitch: "); Serial.print(sensorData.pitch);
+      Serial.print("\tHeading: "); Serial.println(sensorData.heading);
       
-      if (checksum == sensorData.checksum)
-      {Serial.println("Data Integrity PASSED");}
-      else {Serial.println("Data Integrity FAILED");}
+      Serial.print("Battery Temp Left: "); Serial.print(sensorData.battTempLeft);
+      Serial.print("  Battery Temp Right: "); Serial.print(sensorData.battTempRight);
+      Serial.print("  Battery Temp Down: "); Serial.print(sensorData.battTempDown);
+      Serial.print("  Battery Temp Box: "); Serial.print(sensorData.battTempBox);
+      Serial.print("  Temp Cubesat DS18: "); Serial.println(sensorData.TempCubesatDS18);
+      
+      Serial.print("Internal Temp: "); Serial.print(sensorData.temperature);
+      Serial.print("\tPressure: "); Serial.print(sensorData.pressure);
+      Serial.print("\tHumidity: "); Serial.println(sensorData.humidity);
+      
+      Serial.print("GPS Altitude: "); Serial.print(sensorData.GpsAltitude);
+      Serial.print("\tLatitude: "); Serial.print(sensorData.latitude);
+      Serial.print("\tLongitude: "); Serial.print(sensorData.longitude);
+      Serial.print("\tSpeed: "); Serial.print(sensorData.speed);
+      Serial.print("\tSatellites: "); Serial.println(sensorData.numSatellites);
+      
+      Serial.print("Date-Time: ");
+      Serial.print(sensorData.year); Serial.print("-"); Serial.print(sensorData.month); Serial.print("-");
+      Serial.print(sensorData.day); Serial.print(" ");
+      Serial.print(sensorData.hour); Serial.print(":"); Serial.print(sensorData.minute); Serial.print(":");
+      Serial.print(sensorData.second);
+      
+      Serial.print("\tCamera Info: "); Serial.print(sensorData.camera_info);
+
+      Serial.print("\tRssi: ");  Serial.print(LoRa.packetRssi());
+      Serial.print("\tSNR: ");  Serial.println(LoRa.packetSnr());
+      
+      Serial.println();
+
+//      checksum = CRC32::calculate((uint8_t *)&sensorData, sizeof(sensorData) - sizeof(sensorData.checksum));
+      
+//      if (checksum == sensorData.checksum)
+//      {Serial.println("Data Integrity PASSED");}
+//      else {Serial.println("Data Integrity FAILED");}
 
       Serial.println();
     }
